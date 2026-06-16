14edc4c30c43e1cbe15cf90af1dc735425154384
Sriram <parepallisriram9640@gmail.com@gmail.com>
2026-06-16 20:41:37 +0530

    new changes for env file

--- app/config.py
+++ app/config.py
@@ -2,9 +2,12 @@ import os
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from typing import Optional
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+ENV_FILE_PATH = os.path.join(BASE_DIR, ".env")
+
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file=".env",
+        env_file=ENV_FILE_PATH,
         env_file_encoding="utf-8",
         extra="ignore"
     )
